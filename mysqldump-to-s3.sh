2a537043fc07ebe435e97b02afc344f3c28b2b48
Dan Callan <d.callan@library.uq.edu.au>
2017-02-09 14:53:12 +1000

    Added error checking, changed filename, and xz compression prior to upload

--- mysqldump-to-s3.sh
+++ mysqldump-to-s3.sh
@@ -26,17 +26,42 @@ if [[ -z $S3_PREFIX ]] ; then
     exit 1
 fi
 
-DATE=$(date +%Y%m%d%H%M)
+DATE=$(date +%Y%m%d)
 DUMPDIR=/dumps
 DUMPFILE=${DBHOST}_${DBNAME}_${DATE}.sql
-DUMPFILE2=${DBHOST}_${DBNAME}_latest.sql
 
 # dump database
 echo "Dumping ${DBNAME} from ${DBHOST}"
 mysqldump -h ${DBHOST} -u${DBUSER} -p${DBPASS} ${DBNAME} > ${DUMPDIR}/${DUMPFILE}
 
+# test dump file
+if [[ ! -f ${DUMPDIR}/${DUMPFILE} ]]; then
+    echo "ERROR: Dump file ${DUMPDIR}/${DUMPFILE} does not exist (dump upload cancelled).";
+    exit 1;
+fi
+if [[ ! -s ${DUMPDIR}/${DUMPFILE} ]]; then
+    echo "ERROR: Dump file ${DUMPDIR}/${DUMPFILE} is empty (dump upload cancelled).";
+    exit 1;
+fi
+FIRSTLINE=$(head -n1 ${DUMPDIR}/${DUMPFILE})
+if [[ ! $FIRSTLINE == *"MySQL dump"* ]]; then 
+    echo "ERROR: First line of dump file ${DUMPDIR}/${DUMPFILE} does not pass test (dump upload cancelled).";
+    exit 1;
+fi
+LASTLINE=$(tail -n1 ${DUMPDIR}/${DUMPFILE})
+if [[ ! $LASTLINE == *"Dump completed"* ]]; then 
+    echo "ERROR: First line of dump file ${DUMPDIR}/${DUMPFILE} does not pass test (dump upload cancelled).";
+    exit 1;
+fi
+
+# compress dump file
+echo "XZ Compressing dump file ..."
+xz --fast -v ${DUMPDIR}/${DUMPFILE}
+
 # upload dump to s3 (and copy it to "latest" file)
-echo "Uploading ${DUMPFILE} to ${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE}"
-aws s3 cp ${DUMPDIR}/${DUMPFILE} s3://${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE}
-echo "Copying s3://${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE} to ${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE2}"
-aws s3 cp s3://${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE} s3://${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE2}
+echo "Uploading ${DUMPFILE}.xz to ${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE}.xz"
+aws s3 cp ${DUMPDIR}/${DUMPFILE}.xz s3://${S3_BUCKET}/${S3_PREFIX}/${DUMPFILE}.xz
+
+# clean exit
+echo "Dump and upload completed"
+exit 0
